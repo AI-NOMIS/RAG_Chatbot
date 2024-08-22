23e96a633075e650a17a93ec41b0eed3b1312572
Damien <dev@ai-nomis.com>
2024-08-22 16:47:03 +0900

    change sqlite version

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from pydantic import BaseModel
 from typing import List
 import json
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 app = FastAPI()
 
 # class RAGQuery(BaseModel):
