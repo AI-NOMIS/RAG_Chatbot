d60a2617c9e042b5a71a1c245d01ce092245e7b4
Damien <dev@ai-nomis.com>
2024-08-23 16:31:56 +0900

    remove pysqlite

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from pydantic import BaseModel
 from typing import List
 import json
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 app = FastAPI()
 
 # class RAGQuery(BaseModel):
